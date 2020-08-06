d881e80549d1c4043ed5f613dd5596d9d994f290
Tyler <bisig1892@gmail.com>
2020-08-06 14:53:29 -0700

    Created Product Model

---
+++ eCommerceSite/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// A salable product
+    /// </summary>
+    public class Product
+    {
+        public int ProductId { get; set; }
+        /// <summary>
+        /// The consumer facing name of the product
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// The retail price as US currency
+        /// </summary>
+        public double Price { get; set; }
+        /// <summary>
+        /// Category the product falls under. EX. Electronics, Furniture, etc.
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
