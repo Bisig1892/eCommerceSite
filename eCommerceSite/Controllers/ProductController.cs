f239a11bf3641abe6ea89e49a7b61e341d10a566
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2020-08-08 23:32:28 -0700

    Issue#5 add product (#12)
    * Adding product is implemented
    
    * Added Styling to Add Product page
    
    * Added form to add products to the db

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -27,5 +27,28 @@ namespace eCommerceSite.Controllers
             // Send list of products to view to be displayed
             return View(products);
         }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product p)
+        {
+            if(ModelState.IsValid)
+            {
+                //Add to DB
+                _context.Products.Add(p);
+                _context.SaveChanges();
+
+                TempData["Message"] = $"{p.Title} was added successfully!";
+
+                // redirect back to catalog page
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
