df5468f6d8ec5ee3eaff3e755a114e53b7a107c9
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2020-08-20 21:58:06 -0700

    Refactor Db code into separate class (#27)

---
+++ eCommerceSite/Data/ProductDb.cs
@@ -0,0 +1,50 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Data
+{
+    public static class ProductDb
+    {
+        /// <summary>
+        /// Returns the total count of products
+        /// </summary>
+        /// <param name="_context">Database context to use</param>
+        public static async Task<int> GetTotalProductsAsync(ProductContext _context)
+        {
+            return await(from p in _context.Products
+                  select p).CountAsync();
+        }
+        /// <summary>
+        /// Returns a page worth of products
+        /// </summary>
+        /// <param name="_context">Database context to use</param>
+        /// <param name="pageSize">The number of products per page</param>
+        /// <param name="pageNum">Page of products to return</param>
+        public static async Task<List<Product>> GetProductsAsync(ProductContext _context, int pageSize, int pageNum)
+        {
+            return await (from p in _context.Products
+                                            orderby p.Title ascending
+                                            select p)
+                                .Skip(pageSize * (pageNum - 1)) // Skip() must be before Take()
+                                .Take(pageSize)
+                                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Add product to the database
+        /// </summary>
+        /// <param name="_context">Database context to use</param>
+        /// <param name="p">Product to be added</param>
+        public static async Task<Product> AddProductAsync(ProductContext _context, Product p)
+        {
+            //Add to DB
+            _context.Products.Add(p);
+            await _context.SaveChangesAsync();
+            return p;
+        }
+    }
+}
