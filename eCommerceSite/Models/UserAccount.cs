4aa37deb0cc57b22552d88c81b9b2c5325fd089f
Tyler <bisig1892@gmail.com>
2020-08-21 20:36:13 -0700

    Created user account model

---
+++ eCommerceSite/Models/UserAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class UserAccount
+    {
+
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string password { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
