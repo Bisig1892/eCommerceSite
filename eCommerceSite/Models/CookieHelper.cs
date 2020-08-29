9cc49067da21da1a380026282674807dbca91943
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2020-08-29 12:41:05 -0700

    Display cart item count in navigation (#42)

--- eCommerceSite/Models/CookieHelper.cs
+++ eCommerceSite/Models/CookieHelper.cs
@@ -49,7 +49,8 @@ namespace eCommerceSite.Models
 
         public static int GetTotalCartProducts(IHttpContextAccessor http)
         {
-            throw new NotImplementedException();
+            List<Product> cartProducts = getCartProducts(http);
+            return cartProducts.Count;
         }
     }
 }
